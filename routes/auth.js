2a7487934052437e5a7ea0b04c377c4f114eb020
Yasith Wimukthi <yasith@imperiallearning.co.uk>
2023-01-24 22:27:20 +0530

    implement auth controller

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,19 @@
+const {login} = require("../controllers/authController");
 module.exports = function ({ app, dbConn }) {
-  app.post("/login", (req, res) => {
-    const { email, password } = req.body;
-    if (email && password) {
-      const sql = "SELECT * FROM user_account WHERE user_email = ? AND user_password = ?";
-      dbConn.query(sql, [email, password], function (error, response) {
-        if (response && response.length !== 0) {
-          res.status(200).jsonp({ ...response[0] });
-        } else {
-          res.status(200).jsonp({ message: "Your username or password is not correct" });
-        }
-      });
-    } else {
-      res.status(200).jsonp({ message: "Your username or password is not correct" });
-    }
-  });
+  // app.post("/login", (req, res) => {
+  //   const { email, password } = req.body;
+  //   if (email && password) {
+  //     const sql = "SELECT * FROM user_account WHERE user_email = ? AND user_password = ?";
+  //     dbConn.query(sql, [email, password], function (error, response) {
+  //       if (response && response.length !== 0) {
+  //         res.status(200).jsonp({ ...response[0] });
+  //       } else {
+  //         res.status(200).jsonp({ message: "Your username or password is not correct" });
+  //       }
+  //     });
+  //   } else {
+  //     res.status(200).jsonp({ message: "Your username or password is not correct" });
+  //   }
+  // });
+  app.post("/login", login);
 };
