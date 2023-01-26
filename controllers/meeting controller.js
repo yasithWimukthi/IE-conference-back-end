3d28388ce3f9caed7b389c1836a5b2ea81115629
Yasith Wimukthi <yasith@imperiallearning.co.uk>
2023-01-26 20:12:16 +0530

    implement meetings controller

--- controllers/meeting controller.js
+++ controllers/meeting controller.js
@@ -6,6 +6,15 @@ const getMeetingsBelongsToUser  = async (req, res) => {
     });
 }
 
+const getMeetingByMeetingId = async (req, res) => {
+    const id = req.params.id;
+    const findMeetingSql = "SELECT * FROM meeting WHERE meeting_uid = ?";
+    dbConn.query(findMeetingSql, [id], function (error, meeting) {
+        res.status(200).jsonp(meeting);
+    });
+}
+
 module.exports = {
-    getMeetingsBelongsToUser
+    getMeetingsBelongsToUser,
+    getMeetingByMeetingId
 }
